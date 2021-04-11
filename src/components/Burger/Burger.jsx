b99330a0c59184628e869ce0d3a572455dcdb51d
KLIM_nk01 <KLIM_NIKITA_2001@mail.ru>
2021-04-11 20:11:47 +0300

    feat: build project

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -4,6 +4,7 @@ import BurgerStyle from  "./Burger.module.scss";
 function Burger(props) {
 
   return (
+
     <>
       {/*  потом при нажатии добовлять класс is-active  для button */}
         <button onClick={props.onClick} className={BurgerStyle.burger} type="button">
--- src/components/Profile/PersonPost/PostHeader/PostHeader.jsx
+++ src/components/Profile/PersonPost/PostHeader/PostHeader.jsx
@@ -5,12 +5,14 @@ import PostHeaderStyle from "./PostHeader.module.scss";
 
 
 function PostHeader(props) {
-
+    let onClick = () => {
+        props.dispatch({type: 'REMOVE-POST'});
+    }
   return (
     <>
       <div className={PostHeaderStyle.postHeader}>
           <Person imgSrc={'https://prikolist.club/wp-content/uploads/2019/08/kartinki_muzhskie_na_telefon_3_07073155.jpg'} nameSurname={props.nameSurname}/>
-          <Burger onClick={console.log('remove post')}/>
+          <Burger onClick={onClick}/>
       </div>
     </>
   );
